dea8822b02015a41a0534e5e892721af2ba64a64
francoduarte <fduarte@truware.tech>
2026-08-24 20:17:16 -0300

    GREEN: metodo backwards definido e implementado

--- index.ts
+++ index.ts
@@ -82,4 +82,27 @@ export class Rover {
 
         return this.direction;
     }
+
+    moveBackwards() {
+        console.log(`Rover esta mirando en direccion al ${this.direction}`);
+
+        switch (this.direction) {
+            case Orientation.N:
+                this.y--;
+                break;
+            case Orientation.S:
+                this.y++;
+                break;
+            case Orientation.E:
+                this.x--;
+                break;
+            case Orientation.O:
+                this.x++;
+                break;
+        }
+
+        console.log(this.showCoords());
+
+        return this.direction;
+    }
 }
--- test/moveBackwards.test.ts
+++ test/moveBackwards.test.ts
@@ -1,9 +1,27 @@
 import { Orientation, Rover } from '../index';
 
-describe("move forward", () => {
-  test("should move forward", () => {
+describe("move backwards", () => {
+  test("should move backwards", () => {
     const rover = new Rover(0, 0, Orientation.N);
     rover.moveBackwards();
-    expect(rover.showCoords()).toBe("(0, 1, N)");
+    expect(rover.showCoords()).toBe("(0, -1, N)");
+  });
+
+  test("should move backwards", () => {
+    const rover = new Rover(0, 0, Orientation.S);
+    rover.moveBackwards();
+    expect(rover.showCoords()).toBe("(0, 1, S)");
+  });
+
+  test("should move backwards", () => {
+    const rover = new Rover(0, 0, Orientation.E);
+    rover.moveBackwards();
+    expect(rover.showCoords()).toBe("(-1, 0, E)");
+  });
+
+  test("should move backwards", () => {
+    const rover = new Rover(0, 0, Orientation.O);
+    rover.moveBackwards();
+    expect(rover.showCoords()).toBe("(1, 0, O)");
   });
 })
